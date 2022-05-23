e5645c1b8890f92f19b00f20e9bffbe23abdcc01
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-05-23 10:12:03 +0530

    Progress Bar added

--- WindowsFormsIntroductionApp/Form2.Designer.cs
+++ WindowsFormsIntroductionApp/Form2.Designer.cs
@@ -29,11 +29,51 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(83, 38);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(635, 46);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(184, 141);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 36);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Loading...";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form2";
             this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- WindowsFormsIntroductionApp/Form2.cs
+++ WindowsFormsIntroductionApp/Form2.cs
@@ -12,9 +12,28 @@ namespace WindowsFormsIntroductionApp
 {
     public partial class Form2 : Form
     {
+        static int flag;
         public Form2()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (flag <= 100)
+            {
+                progressBar1.Value = flag;
+                flag++;
+            }
+            else
+            {
+                timer1.Enabled = false;
+                Form1 frm = new Form1();
+                frm.Show();
+                
+            }
+
+
         }
     }
 }
