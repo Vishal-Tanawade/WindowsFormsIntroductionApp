22b6885979fb957983413ba0e277efa062858a1c
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-05-23 08:09:21 +0530

    Created Windows form App 1) add label-> enter Name, and text 2) add textbox-> enter name 3) add button-> enter text and name 4) create event for btnclick-> show messagebox

--- WindowsFormsIntroductionApp/Form1.Designer.cs
+++ WindowsFormsIntroductionApp/Form1.Designer.cs
@@ -28,13 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnGreet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblName.Location = new System.Drawing.Point(64, 77);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(198, 29);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Enter Your Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(280, 71);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(270, 35);
+            this.txtName.TabIndex = 1;
+            // 
+            // btnGreet
+            // 
+            this.btnGreet.BackColor = System.Drawing.SystemColors.Control;
+            this.btnGreet.Location = new System.Drawing.Point(582, 69);
+            this.btnGreet.Name = "btnGreet";
+            this.btnGreet.Size = new System.Drawing.Size(114, 37);
+            this.btnGreet.TabIndex = 2;
+            this.btnGreet.Text = "Greet Me";
+            this.btnGreet.UseVisualStyleBackColor = false;
+            this.btnGreet.Click += new System.EventHandler(this.btnGreet_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGreet);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnGreet;
     }
 }
 
--- WindowsFormsIntroductionApp/Form1.cs
+++ WindowsFormsIntroductionApp/Form1.cs
@@ -16,5 +16,13 @@ namespace WindowsFormsIntroductionApp
         {
             InitializeComponent();
         }
+
+     
+
+        private void btnGreet_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello, "+txtName.Text);
+
+        }
     }
 }
